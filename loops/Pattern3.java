a666b2e98f3e9a411c1015ec8c81e6c701bf8db2
Devansh Singh Gaharwar <sdevansh021@gmail.com>
2024-10-20 00:29:18 +0530

    pattern 3

---
+++ loops/Pattern3.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class Pattern3 
+{
+    public static void main(String[] args) {
+            for(int i=6;i>=3;i--)
+            {
+                for(int j=2;j<i;j++)
+                {
+                    System.out.print(i);
+     }
+                System.out.println();
+            }
+        }
+    }
+    
+    
+
