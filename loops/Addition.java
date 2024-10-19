f2cfb4d0d7851c71214db65b99ab192d27b607ed
Devansh Singh Gaharwar <sdevansh021@gmail.com>
2024-10-19 13:37:50 +0530

    Addition

--- loops/Addition.java
+++ loops/Addition.java
@@ -3,7 +3,12 @@ package loops;
 public class Addition {
 
     public static void main(String[] args) {
-        System.out.println("in addition");
+        int b=2;
+        for (int a=1;a<=5;a++)
+        {
+         a= a+b;
+           System.out.println(a);
+        }
     }
     
 }
