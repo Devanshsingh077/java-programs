75427f6c2947e53ffdbedbd9a8ed60f3aba0a74b
Devansh Singh Gaharwar <sdevansh021@gmail.com>
2024-10-19 12:55:17 +0530

    java programs

---
+++ Hello.java
@@ -0,0 +1,27 @@
+
+public class Hello {
+
+    public static class Devansh {
+
+        public static void add()
+        {
+            int a=70;
+            int b=40;       
+            System.out.println(a+b);
+        
+        }
+    }
+
+    public static void main(String[] args) {
+        
+
+        Devansh.add();
+        
+       
+
+
+
+        // Devansh.add();
+    }
+
+}
